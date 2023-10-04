5421a686178a796b14183927d186bc6bb5863312
arwegasira <ac.rwegasira@gmail.com>
2023-10-04 17:51:10 +0200

    typo error

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import AppContext from './context'
+import AppContext from './Context'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
